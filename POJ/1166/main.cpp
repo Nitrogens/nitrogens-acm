92353b4ecb4ebc570ca2a1c4e29b44e118e4b58e
Nitrogens <xumingjies@163.com>
2018-02-05 20:15:37 +0800

    add explanation to POJ1166
    add explanation to POJ1166

--- POJ/1166/main.cpp
+++ POJ/1166/main.cpp
@@ -1,7 +1,11 @@
 #include <cstdio>
 #include <cstring>
 
-char clock[10], clock_original[10];
+//用于存储原始的钟表数据
+char clock_original[10];
+//每次操作，都要从原始数据中复制一份，再进行操作
+char clock[10];
+//为书写简便，将（编号）1~9移动封装成一个函数
 void operation(int number, int frequency);
 int main(void)
 {
@@ -12,6 +16,12 @@ int main(void)
 		scanf("%d", &clock_original[i]);
 	}
 	
+	/*
+		对所有的移动进行枚举，每种移动最多进行三次
+		也就是对每种移动的次数进行遍历 
+		
+		如果进行四次的话，等于没有进行 
+	*/ 
 	for(i1=0; i1<=3; i1++)
 	for(i2=0; i2<=3; i2++)
 	for(i3=0; i3<=3; i3++)
@@ -22,8 +32,10 @@ int main(void)
 	for(i8=0; i8<=3; i8++)
 	for(i9=0; i9<=3; i9++)
 	{
+		//每一种方案，都要从原始数据复制一份进行操作 
 		memcpy(clock, clock_original, sizeof(clock_original));
 		
+		//对钟表进行移动操作 
 		operation(1, i1);
 		operation(2, i2);
 		operation(3, i3);
@@ -34,8 +46,10 @@ int main(void)
 		operation(8, i8);
 		operation(9, i9);
 		
+		//如果所有的钟表都指向12 
 		if(clock[1]+clock[2]+clock[3]+clock[4]+clock[5]+clock[6]+clock[7]+clock[8]+clock[9] == 0)
 		{
+			//从小到大输出，有几步移动就输出几步 
 			for(i=1; i<=i1; i++)	printf("1 ");
 			for(i=1; i<=i2; i++)	printf("2 ");
 			for(i=1; i<=i3; i++)	printf("3 ");
@@ -54,6 +68,7 @@ int main(void)
 	return 0;
 }
 
+//number表示移动编号，frequency表示操作次数 
 void operation(int number, int frequency)
 {
 	int i;
