1747501facc3d71aee46157356b41e8a4b7bd433
Nitrogens <xumingjies@163.com>
2018-03-31 21:29:45 +0800

    Add annotation of POJ2080
    Add annotation of POJ2080

--- POJ/2080/main.cpp
+++ POJ/2080/main.cpp
@@ -8,6 +8,8 @@ bool isLeapYear(int year);
 int main(void)
 {
 	int input, year, month, day, i, weekday, backup;
+	
+	//查表法 
 	int monthsOfLeapYear[12]
 	{
 		31,
@@ -58,10 +60,14 @@ int main(void)
 		day = 0;
 		if(input == -1)
 			break;
+		//从1999年12月31日开始计算的话，比较方便，因此当作从这天开始，input需要加1 
 		input++;
 		backup = input;
 		while(true)
 		{
+			/*
+				前两个if都是处理year，不断减去每一年的天数 
+			*/
 			if(isLeapYear(year) && input > 366)
 			{
 				input -= 366;
@@ -76,10 +82,16 @@ int main(void)
 			}
 			else
 			{
+				//开始处理月份和日期 
 				for(i=0; i<12; i++)
 				{
 					if(isLeapYear(year))
 					{
+						/* 
+							这里必须用<=, 而不是<, 否则会出现类似 2001-01-00 这类的错误答案
+							正确答案是2000-12-31
+							往下以此类推 
+						*/ 
 						if(input <= monthsOfLeapYear[i])
 							break;
 					}
@@ -95,21 +107,21 @@ int main(void)
 					if(i > 0)
 						day = input - monthsOfLeapYear[i-1];
 					else
-						day = input;
+						day = input;	//1月
 				}
 				else
 				{
 					if(i > 0)
 						day = input - monthsOfNormalYear[i-1];
 					else
-						day = input;
+						day = input;	//1月
 				}
 				break;
 			}
 			
 		}
-		weekday = (backup % 7 + 5) % 7;
-		printf("%04d-%02d-%02d ", 2000+year, month, day);
+		weekday = (backup % 7 + 5) % 7;	//计算星期，基准时间是1999年12月31日 星期五 
+		printf("%04d-%02d-%02d ", 2000+year, month, day);	//注意按照格式输出 
 		cout<<weekDay[weekday]<<endl;
 	}
 	
@@ -117,5 +129,6 @@ int main(void)
 }
 bool isLeapYear(int year)
 {
+	//闰年的判断，4年一闰，100年不闰，400年又闰 
 	return (year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0));
 }
